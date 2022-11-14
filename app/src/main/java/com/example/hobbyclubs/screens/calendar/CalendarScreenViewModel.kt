f5e614455a7e8fc96ba91c62806cc1d3ddbecf9b
Miro Taxell <taxell.miro@gmail.com>
2022-11-14 11:18:01 +0200

    mock data

--- app/src/main/java/com/example/hobbyclubs/screens/calendar/CalendarScreenViewModel.kt
+++ app/src/main/java/com/example/hobbyclubs/screens/calendar/CalendarScreenViewModel.kt
@@ -8,16 +8,17 @@ import java.time.LocalDate
 class CalendarScreenViewModel() : ViewModel() {
     private val selectionFlow = MutableStateFlow(emptyList<LocalDate>())
 
+    // MOCK DATA
     val eventsFlow = MutableStateFlow(
         listOf(
-            Event(LocalDate.now(), "Event Today", false, false),
-            Event(LocalDate.now().plusDays(1), "Tournament 1", true, false),
-            Event(LocalDate.now().plusDays(3), "Tournament 2", false, false),
-            Event(LocalDate.now().plusDays(5), "Tournament 3", true, true),
-            Event(LocalDate.now().plusDays(-2), "Tournament 4 - 1", false, true),
-            Event(LocalDate.now().plusDays(-2), "Tournament 4 - 2", false, true),
-            Event(LocalDate.now().plusDays(-2), "Tournament 4 - 3", false, true),
-            Event(LocalDate.now().plusDays(-2), "Tournament 4 - 4", false, true),
+            Event(LocalDate.now(), "Ice Hockey Tournament", false, false),
+            Event(LocalDate.now().plusDays(1), "Chess Game Night", true, false),
+            Event(LocalDate.now().plusDays(3), "Ice Hockey Tournament", false, false),
+            Event(LocalDate.now().plusDays(5), "Board Games Club - Game Night", true, true),
+            Event(LocalDate.now().plusDays(-2), "Ice Hockey Tournament", false, true),
+            Event(LocalDate.now().plusDays(-2), "Table Tennis Tournament", true, false),
+            Event(LocalDate.now().plusDays(-2), "Tennis Tournament", true, true),
+            Event(LocalDate.now().plusDays(-2), "Chess Tournament", false, false),
             )
     )
 
