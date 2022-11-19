e10c50ef761b0fe3e074bb6cc001d3fdbf026b0f
Jonathan Schmidt <scjo1087@h-ka.de>
2022-11-19 13:19:08 +0200

    added drawer to homescreen to see the ui

--- app/src/main/java/com/example/hobbyclubs/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/hobbyclubs/screens/home/HomeScreen.kt
@@ -1,40 +1,63 @@
 package com.example.hobbyclubs.screens.home
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.hobbyclubs.navigation.NavRoutes
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavController) {
     Surface() {
-        Column(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.fillMaxSize(),
-        ) {
-            FakeButtonForNavigationTest(destination = "News") {
-                navController.navigate(NavRoutes.NewsScreen.route)
+        val drawerState = rememberDrawerState(DrawerValue.Closed)
+        val scope = rememberCoroutineScope()
+        val items = listOf("Home", "Restaurant", "HobbyClubs", "Parking", "Settings", "Profile")
+        val selectedItem = remember { mutableStateOf(items[2]) }
+        ModalNavigationDrawer(drawerState = drawerState, drawerContent = {
+            ModalDrawerSheet {
+                Spacer(Modifier.height(12.dp))
+                items.forEach { item ->
+                    NavigationDrawerItem(
+                        label = { Text(item) },
+                        selected = item == selectedItem.value,
+                        onClick = {
+                            scope.launch { drawerState.close() }
+                        },
+                        modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                    )
+                }
             }
-            FakeButtonForNavigationTest(destination = "Calendar") {
-                navController.navigate(NavRoutes.CalendarScreen.route)
+        }, content = {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize(),
+            ) {
+                Button(onClick = { scope.launch { drawerState.open() } }) {
+                    Text("Click to open")
+                }
+                FakeButtonForNavigationTest(destination = "News") {
+                    navController.navigate(NavRoutes.NewsScreen.route)
+                }
+                FakeButtonForNavigationTest(destination = "Calendar") {
+                    navController.navigate(NavRoutes.CalendarScreen.route)
+                }
+                FakeButtonForNavigationTest(destination = "Club Page") {
+                    navController.navigate(NavRoutes.ClubPageScreen.route)
+                }
+                FakeButtonForNavigationTest(destination = "Clubs") {
+                    navController.navigate(NavRoutes.ClubsScreen.route)
+                }
             }
-            FakeButtonForNavigationTest(destination = "Club Page") {
-                navController.navigate(NavRoutes.ClubPageScreen.route)
-            }
-            FakeButtonForNavigationTest(destination = "Clubs") {
-                navController.navigate(NavRoutes.ClubsScreen.route)
-            }
-        }
+        })
     }
 }
 
