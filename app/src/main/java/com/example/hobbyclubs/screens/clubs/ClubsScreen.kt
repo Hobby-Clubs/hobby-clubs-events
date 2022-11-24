a4337d917c8b273bc23adc938d2f2c360766d1f0
Jonathan Schmidt <scjo1087@h-ka.de>
2022-11-24 09:32:35 +0200

    adjusted colors on ClubScreen

--- app/src/main/java/com/example/hobbyclubs/screens/clubs/ClubsScreen.kt
+++ app/src/main/java/com/example/hobbyclubs/screens/clubs/ClubsScreen.kt
@@ -1,7 +1,6 @@
 package com.example.hobbyclubs.screens.clubs
 
 import android.net.Uri
-import android.util.Log
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
@@ -11,9 +10,9 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
+import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -141,8 +140,8 @@ fun ClubTile(
     Card(
         modifier = modifier.clickable { onClick() },
         shape = RoundedCornerShape(12.dp),
-        border = BorderStroke(1.dp, clubTileBorder),
-        colors = CardDefaults.cardColors(clubTileBg)
+        border = BorderStroke(1.dp, colorScheme.outlineVariant),
+        colors = CardDefaults.cardColors(colorScheme.surface)
     ) {
         Row(
             modifier = Modifier
