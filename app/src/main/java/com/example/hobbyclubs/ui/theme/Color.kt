489a7eeb17cd004d60bf9598fc9b21a913759587
Jonathan Schmidt <scjo1087@h-ka.de>
2022-11-24 10:20:58 +0200

    removed unused colors from Color

--- app/src/main/java/com/example/hobbyclubs/ui/theme/Color.kt
+++ app/src/main/java/com/example/hobbyclubs/ui/theme/Color.kt
@@ -64,13 +64,4 @@ val md_theme_dark_outlineVariant = Color(0xFF44464F)
 val md_theme_dark_scrim = Color(0xFF000000)
 
 val suggestedEventColor = Color(0xFF006A60)
-val nokiaBlue = Color(0xFF124191)
-val nokiaDarkBlue = Color(0xFF253991)
-val nokiaLighterBlue = Color(0xFF4895F8)
 val linkBlue = Color(0xFF3366CC)
-val joinedColor = Color(0xFF4CAF50)
-val clubTileBg = Color(0xFFFEFBFF)
-val clubTileBorder = Color(0xFFCAC4D0)
-val liked = Color(0xFF725572)
-
-val forestGreen = Color(0xFF228B22)
